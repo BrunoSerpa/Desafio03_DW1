05de0674ff7f99ffa1949f947ff1f608e9550800
Bruno Serpa <79608249+BrunoSerpa@users.noreply.github.com>
2023-04-30 17:28:51 -0300

    vinculei bd com app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_mysqldb import MySQL
+
 app = Flask("__name__")
+
+app.config["MYSQL_Host"] = "127.0.0.1"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "fatec"
+app.config["MYSQL_DB"] = "Unes"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def home():
@@ -15,4 +24,12 @@ def quemSomos():
 
 @app.route("/contato")
 def contato():
+    if request.method == "POST":
+        ema, ass, des= request.form["email"],  request.form["assunto"], request.form["descricao"]
+
+        conn = mysql.connection
+        conn.cursor().execute(f"insert into Contato(email_contato, assunto_contato, descricao_contato) values('{ema}', '{ass}', '{des}');")
+        conn.commit()
+        conn.cursor().close()
+
     return render_template("contato.html")
