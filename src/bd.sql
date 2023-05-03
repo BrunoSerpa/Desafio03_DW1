b8ad3da5f50867d32e39af3a026d15e3d8dbc6a2
Bruno Serpa <79608249+BrunoSerpa@users.noreply.github.com>
2023-05-03 19:24:10 -0300

    arrumei bd

--- src/bd.sql
+++ src/bd.sql
@@ -1,9 +1,10 @@
-create database Unes;
+use nomeDoDataBase
 
 create table Contato(
 	email_contato varchar(70) not null,
     assunto_contato varchar(100) not null,
-    descricao_contato varchar(500) not null
+    descricao_contato varchar(500) not null,
+    estado char(1) not null
 );
 
 select * from Contato;
