a12c38afcf8843bd5efb83ffac7fa72a2389ef16
Bruno Serpa <79608249+BrunoSerpa@users.noreply.github.com>
2023-05-03 19:22:37 -0300

    add  app.route "users"

--- src/app.py
+++ src/app.py
@@ -5,12 +5,12 @@ app = Flask("__name__")
 
 app.config["MYSQL_Host"] = "127.0.0.1"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "fatec"
-app.config["MYSQL_DB"] = "Unes"
+app.config["MYSQL_PASSWORD"] = "senhaDoSeuRoot"
+app.config["MYSQL_DB"] = "nomeDoDataBase"
 
 mysql = MySQL(app)
 
-@app.route("/")
+@app.route("/")  
 def home():
     return render_template("index.html")
 
@@ -31,5 +31,14 @@ def contato():
         conn.cursor().execute(f"insert into Contato(email_contato, assunto_contato, descricao_contato) values('{ema}', '{ass}', '{des}');")
         conn.commit()
         conn.cursor().close()
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        usersDetails = cur.fetchall()
+        return render_template('user.html', usersDetails=usersDetails)
 
     return render_template("contato.html")
